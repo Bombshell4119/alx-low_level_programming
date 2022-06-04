ba5b55f0c44fc158d4b4d34bbc29157a861ba1b5
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-03 21:14:28 -0700

    great

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+   /* use the function put
+    * you re not allowed to use printf
+    * program must end with 0
+    */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
