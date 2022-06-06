7910888506bc177aa2ab5a17d4e9f3d03ef062db
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-06 16:46:28 -0700

    hhhd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-*main- Entry point
-*Return: 1 always
+* main - entry point
+* Return: 1
 */
+
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
