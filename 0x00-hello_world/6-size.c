436b785bc3431372ad074d78546bc931fbd7920d
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-06 10:39:17 -0700

    success

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ float floatType;
 double doubleType;
 char charType;
 printf("size of int: %zu bytes\n", sizeof(intType));
-printf("size of float: %zu bytes\n", sizeof(floarType));
+printf("size of float: %zu bytes\n", sizeof(floatType));
 printf("size of double: %zu bytes\n", sizeof(doubleType));
 printf("size of char: %zu bytes\n", sizeof(charType));
 return (0);
