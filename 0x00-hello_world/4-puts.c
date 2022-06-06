6432ff2ce0c6b9ef8a871544b142867f51686ac3
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-05 19:55:29 -0700

    this was so great

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
-/* use the
-* you re not allowed to use printf
-* program must end with 0
+/*
+*evrything hass been
+*entered good order
 */
 
-int main (void)
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\"");
-    
-  return(0);
+puts("\" Programming is like building a multilingual puzzle \"");
+return (0);
 }
