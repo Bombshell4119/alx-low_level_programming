d6fce8434d0cc9e1e72b225186cfddf06647b17c
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-08 18:25:52 -0700

    again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,16 @@
 #include "holberton.h"
-
 /**
-
- * main - Entry point to print the string.
-
- * Return: 0
-
- */
-
+* main - Entry point to print the string.
+* Return: 0
+*/
 int main(void)
-  
 {
-  
-  char c[] = "Holberton";
-  
-  int i;
-  
-
-  
-  for (i = 0; i < 9; i++)
-    
-    {
-      
-      _putchar(c[i]);
-      
-    }
-  
-  _putchar('\n');
-  
-  return (0);
-  
+char c[] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');  
+return (0);
 }
