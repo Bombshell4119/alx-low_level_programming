2b6acc13f984630f70d8aff50026ecc6884010f5
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-22 06:48:37 -0700

    save

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <holberton.h>
+/**
+*_print_rev_recursion - prints out a string
+*@s: print out the string
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
