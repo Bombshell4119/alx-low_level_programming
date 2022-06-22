485e48563944429bf339f7670c7c5e2fff8c13f0
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-22 07:12:10 -0700

    love

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+* _strlen_recursion - returns as the length of a string
+* s: string to count as integers
+* Return: length of the string
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+s++;
+return (1 + _strlen_recursion(s));
+}
