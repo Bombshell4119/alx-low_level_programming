aee2e050033305b49802c766842e20c2c8d28a81
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-22 08:28:00 -0700

    love

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,12 +12,12 @@ return (n);
 if (n * n > base)
 return (-1);
 return (checker(n + 1, base));
+}
 /**
 *_sqrt_recursion - return the natural square root of a number n.
 *@n: number to check for square roots.
 *Return: the natural square root of number n
 */
-}
 int _sqrt_recursion(int n)
 {
 return (checker(1, n));
