1e0fc60e86be14ec054df5012cd2e57e2a43c256
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-22 08:21:45 -0700

    love

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,7 @@
 *checker -checks the input number from n to the base
 *@n:number is squared and compared against base
 *@base:base number to check
-*Return:natural square root of number base and number n
-*_sqrt_recursion -This return the natural aquare root of any number
-*@n:This is the number to check for square roots
+*Return:natural square root of number base
 */
 int checker(int n, int base)
 {
@@ -14,6 +12,11 @@ return (n);
 if (n * n > base)
 return (-1);
 return (checker(n + 1, base));
+/**
+*_sqrt_recursion - return the natural square root of a number n.
+*@n: number to check for square roots.
+*Return: the natural square root of number n
+*/
 }
 int _sqrt_recursion(int n)
 {
