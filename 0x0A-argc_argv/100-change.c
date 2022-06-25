66f0f20f52c430b62e0ad314cdf259d81f2bf1bf
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-06-25 06:08:02 -0700

    again

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,55 +1,61 @@
+#include "holberton.h"
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 /**
-* checker - checks for valid input
-* @argc: argument count
-* @i: counter for argv[]
-* @j: counter for argv[][]
-* @argv: argument vector
-* Return: 0 on success, 1 on failure
+* coinConverter - Helper function that does all the mathematics
+* @i: Passed in variable from main for calculations
+* Return: The number of coins needed minimum for the passed in variable
 */
-int checker(int argc, int i, unsigned int j, char *argv[])
+int coinConverter(int i)
 {
-for (i = 1; i <= argc; i++)
-for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-if (isdigit(argv[i][j]) == 0)
-return (1);
-return (0);
+int count = 0;
+while (i != 0)
+{
+if (i % 10 == 9 || i % 10 == 7)
+i -= 2;
+else if (i % 25 == 0)
+i -= 25;
+else if (i % 10 == 0)
+i -= 10;
+else if (i % 5 == 0)
+i -= 5;
+else if (i % 2 == 0)
+{
+if (i % 10 == 6)
+i -= 1;
+else
+i -= 2;
+}
+else
+i -= 1;
+count++;
+}
+return (count);
 }
 /**
-* main - Prints the minimum number of coins
-* to make change for an amount of cents.
-* @argc: argument count
-* @argv: argument vector
-* Return: 0 on success
+* main - Takes in exactly one argument for minimum coin count
+* @argc: Number of command line arguments
+* @argv: Array name
+* Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
 */
 int main(int argc, char *argv[])
 {
-unsigned int cents;
-int coins;
-cents = coins = 0;
-if (argc == 2)
+int i, coin;
+coin = 0;
+if (argc != 2)
 {
-if (argv[1][0] == '-')
-printf("0\n");
-if (checker(argc, 1, 0, argv) == 0)
-{
-cents = atoi(argv[1]);
-for ( ; cents >= 25; coins++, cents -= 25)
-;
-for ( ; cents >= 10; coins++, cents -= 10)
-;
-for ( ; cents >= 5; coins++, cents -= 5)
-;
-for ( ; cents >= 2; coins++, cents -= 2)
-;
-for ( ; cents >= 1; coins++, cents--);
-printf("%d\n", coins);
-}
-}
-else
 printf("Error\n");
+return (1);
+}
+i = atoi(argv[1]);
+if (i < 0)
+printf("0\n");
+else
+{
+coin = coinConverter(i);
+printf("%d\n", coin);
+}
 return (0);
 }
