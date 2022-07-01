d338c073aab15f66b733cfc8d578720512246ad3
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-07-01 09:35:36 -0700

    hate

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
 *_memset - fills memory with a constant byte
-* @s: pointer to put the constant
-* @b: constant
-* @n: max bytes to use
-* Return: s
+*@s: pointer to put the constant
+*@b: constant
+*@n: max bytes to use
+*Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -17,18 +17,18 @@ return (s);
 }
 /**
 *_calloc - allocates memory for an array, using malloc
-* @nmemb: array length
-* @size: size of each element
-* Return: pointer
+*@nmemb: array length
+*@size: size of each element
+*Return: pointer
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *m;
 if (nmemb == 0 || size == 0)
-return (NULL); 
-m = malloc(nmemb * size);  
+return (NULL);
+m = malloc(nmemb * size);
 if (m == 0)
 return (NULL);
- _memset(m, 0, (nmemb * size));
-return (m);  
+_memset(m, 0, (nmemb * size));
+return (m);
 }
