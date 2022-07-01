ade7505e292333b87f457b4dfa6beed01cf5445a
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-07-01 09:29:50 -0700

    same

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-* _strlen - find length of a string
-* @s: string
-* Return: int
+*_strlen - find length of a string
+*@s: string
+*Return: int
 */
 unsigned int _strlen(char *s)
 {
@@ -12,11 +12,11 @@ for (; s[size] != '\0'; size++)
 return (size);
 }
 /**
-*string_nconcat - concatenates two strings
-* @s1: string 1
-* @s2: string 2
-* @n: first bytes of s2 to be used
-* Return: pointer or NULL
+*string_nconcat - This concatenates two strings
+*@s1: this is string 1
+*@s2: this is string 2
+*@n: this is the first bytes of s2 to be used
+*Return: pointer or NULL
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -26,7 +26,7 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (n < _strlen(s2))    
+if (n < _strlen(s2))
 m = malloc(_strlen(s1) + n * sizeof(char) + 1);
 else
 m = malloc(_strlen(s1) + _strlen(s2) + 1);
@@ -36,6 +36,6 @@ for (i = 0; s1[i] != '\0'; i++)
 m[i] = s1[i];
 for (j = 0; s2[j] != '\0' && j < n; i++, j++)
 m[i] = s2[j];
-m[i] = '\0'; 
+m[i] = '\0';
 return (m);
 }
