a82a50019e64a51aee6e1f4f2d2d7fe99f0fc3bc
Bombshell4119 <adedayoandadetunji@gmail.com>
2022-07-08 16:46:32 -0700

    this is a sample code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
 * sum_them_all - function with two arguments
 * @n: int type first argume t
@@ -8,14 +9,16 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list valist;
-unsigned int i;
-int sum = 0;
-if (n == 0)
-return (0);
-va_start(valist, n);
-for (i = 0; i < n; i++)
-sum += va_arg(valist, int);
-va_end(valist);
-return (sum);
+  unsigned int sum = 0;
+  unsigned int i;
+  va_list arguments;
+
+  va_start(arguments, n);
+  if (n == 0)
+    return (0);
+  for (i = 0; i < n; i++)
+    {
+      sum += va_arg(argumnets, unsigned int);
+    }
+  return (sum);
 }
