0329843a15064b9126ae4b111d9a70b80eaf2400
Bombshell4119 <ageneralsam2@gmail.com>
2022-07-14 16:00:41 -0700

    this is the corrected repo

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-* create_array - writes the character c to stdout
-* @size: The character to print
-* @c: The character to print
-* Return: pointer to array or null
+*create_array - writes the character c to stdout
+*@size: The character to print
+*@c: The character to print
+*Return: pointer to array or null
 */
 char *create_array(unsigned int size, char c)
 {
@@ -13,7 +13,7 @@ unsigned int i;
 if (size == 0)
 {
 return (NULL);
-}  
+}
 q = malloc(sizeof(char) * size);
 if (q == NULL)
 {
