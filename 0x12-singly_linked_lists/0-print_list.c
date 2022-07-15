3669114adad118e38ee52800c6c74f21037de5c9
Bombshell4119 <ageneralsam2@gmail.com>
2022-07-14 17:18:07 -0700

    another level

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
 * print_list - prints all the elements of a list_t list.
 * @h: singly linked list.
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 /**
  * list_len - returns then number of elements in a list.
