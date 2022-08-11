45d52e0f63aa163329e53c756083f7c54c1f7f5b
Bombshell4119 <ageneralsam2@gmail.com>
2022-08-11 02:52:50 -0700

    movement

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,69 +1,91 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
 /**
-* new_dog - function that creates a new dog
-* @name: passed from main
-* @age: passed from main
-* @owner: passed from main
-* Return: pointer to the struct
-*/
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *ptr;
-if (name == NULL || owner == NULL)
-return (NULL);
-ptr = malloc(sizeof(dog_t));
-if (ptr != NULL)
-{
-ptr->name = _strdup(name);
-if (ptr->name == NULL)
-{	  
-free(ptr->name);
-free(ptr);
-return (NULL);
-}
-ptr->age = age;
-ptr->owner = _strdup(owner);
-if (ptr->owner == NULL)
-{
-free(ptr->name);
-free(ptr->owner);
-free(ptr);
-return (NULL);
-}
-return (ptr);
-}
-return (NULL);
-}
-/**
-* _strdup - function that returns a pointer to a newly allocated space
-* in memory, which contains a copy of the string given as a parameter
-* @str: string of chars
-* Return: address of the newly allocated memory
-*/
-char *_strdup(char *str)
-{
-unsigned int len;
-unsigned int i, j;
-copy;
-char *tmp = str;
-if (str == NULL)
-return (NULL);
-i = 0;
-while (*str++)
-i++;
-len = i;
-str = tmp;
-str_copy = malloc(len * sizeof(char) + 1);
-if (str_copy == NULL)
-return (NULL);
-j = 0;
-while (j < len)
-{
-str_copy[j] = str[j];
-j++;
-} 
-str_copy[j] = '\0';
-return (str_copy);
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
