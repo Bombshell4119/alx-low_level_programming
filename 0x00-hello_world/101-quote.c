7e5a66725b4a8240bea85a9730c3be727933c2ce
Bombshell4119 <ageneralsam2@gmail.com>
2022-08-11 16:00:38 -0700

    same

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
-* main - entry point
-* Return: 1
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 1(Success)
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
 return (1);
 }
